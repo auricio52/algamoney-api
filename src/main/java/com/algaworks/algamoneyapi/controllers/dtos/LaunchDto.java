3f6c57223ac14749784feadf7313a53aebd6ccac
auricio52 <auricio52@gmail.com>
2023-01-03 20:49:38 -0300

    Add launch validations

--- src/main/java/com/algaworks/algamoneyapi/controllers/dtos/LaunchDto.java
+++ src/main/java/com/algaworks/algamoneyapi/controllers/dtos/LaunchDto.java
@@ -2,20 +2,27 @@ package com.algaworks.algamoneyapi.controllers.dtos;
 
 import com.algaworks.algamoneyapi.entities.enums.LaunchType;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record LaunchDto (
      Long id,
+     @NotNull
      String description,
+     @NotNull
      @JsonFormat(pattern =  "dd/MM/yyyy")
      LocalDate expirationDate,
      @JsonFormat(pattern =  "dd/MM/yyyy")
      LocalDate paymentDate,
+     @NotNull
      BigDecimal value,
      String note,
+     @NotNull
      LaunchType type,
+     @NotNull
      CategoryDto category,
+     @NotNull
      PersonDto person
 ) { }
